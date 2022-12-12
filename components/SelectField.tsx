ea0887e191a335e88734093d23dc86e331b8b738
Carlos Daniel <ruuufffaria@gmail.com>
2022-12-12 19:09:20 -0300

    first commit

---
+++ components/SelectField.tsx
@@ -0,0 +1,36 @@
+interface SelectFieldProps {
+  id: string;
+  label: string;
+  fields: string[];
+  value: string;
+  onChange: any;
+}
+
+export default function SelectField({
+  id,
+  label,
+  fields,
+  value,
+  onChange,
+}: SelectFieldProps) {
+  return (
+    <div className="pb-4">
+      <label className="block font-latoBold text-sm pb-2" htmlFor={id}>
+        {label}
+      </label>
+      <select
+        id={id}
+        name={id}
+        value={value}
+        onChange={onChange}
+        className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 outline-none"
+      >
+        {fields.map((field, index) => (
+          <option key={index} value={field}>
+            {field}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        latoRegular: ["Lato Regular", "sans-serif"],
+        latoBold: ["Lato Bold", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
